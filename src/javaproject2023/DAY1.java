5e4588158ae57cb955869beabf3ae70bbbac8e06
sandeshshanu <129872524+sandeshshanu@users.noreply.github.com>
2023-04-10 21:01:03 +0530

    DAY3
    STATIC VOID AND NON STATIC VOID

---
+++ src/javaproject2023/DAY1.java
@@ -0,0 +1,13 @@
+package javaproject2023;
+
+public class DAY1 {
+	public static void main(String[] args) {
+
+		
+addfunction(20,30);
+	}
+public static void addfunction (int a , int b )
+{
+	System.out.println(a+b);
+}
+}
